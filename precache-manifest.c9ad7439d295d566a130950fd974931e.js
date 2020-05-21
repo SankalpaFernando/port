1d4979061f35fa05b9815d796ba96056b9525a70
SankalpaFernando <sankalpafernando2017@gmail.com>
2020-05-21 13:31:22 +0530

    Updates

---
+++ precache-manifest.c9ad7439d295d566a130950fd974931e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98f104189d017266096f08de3e3031c",
+    "url": "/port/index.html"
+  },
+  {
+    "revision": "794fd152cc79ada20dc9",
+    "url": "/port/static/css/main.d809746f.chunk.css"
+  },
+  {
+    "revision": "d0733edeb8702f315658",
+    "url": "/port/static/js/2.5f9e7567.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/port/static/js/2.5f9e7567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "794fd152cc79ada20dc9",
+    "url": "/port/static/js/main.b1f2a7cd.chunk.js"
+  },
+  {
+    "revision": "30ff79d3aaa4d896cd61",
+    "url": "/port/static/js/runtime-main.dd51b5be.js"
+  },
+  {
+    "revision": "11a1de5de2d37ad9581a15ae74b6fd48",
+    "url": "/port/static/media/avatar.11a1de5d.png"
+  },
+  {
+    "revision": "f92e4ea99f2013ffafdd998664f561d6",
+    "url": "/port/static/media/hotel.f92e4ea9.png"
+  },
+  {
+    "revision": "36367b81a083584a93b6de4a86ea8174",
+    "url": "/port/static/media/image.36367b81.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/port/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1f91d0a095e6f6bd1fddf544689924bb",
+    "url": "/port/static/media/movie.1f91d0a0.png"
+  }
+]);
